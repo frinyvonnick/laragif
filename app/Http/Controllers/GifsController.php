ee07c86925ca69af8de47bfdcdcc62503512de4d
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-04-11 22:32:38 +0200

    :sparkles: Set up trending api

---
+++ app/Http/Controllers/GifsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\GiphyRepository;
+
+class GifsController extends Controller
+{
+	const LIMIT = 20;
+
+	private $giphy;
+
+	public function __construct(GiphyRepository $giphy) {
+			$this->giphy = $giphy;
+	}
+
+	public function trending(int $offset) {
+		return json_decode($this->giphy->trending(self::LIMIT, $offset))->data;
+	}
+
+  public function index() {
+		return view('index', [
+			'gifs' => json_decode($this->giphy->trending(self::LIMIT, 0))->data
+		]);
+	}
+}
