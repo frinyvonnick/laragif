34b89b0d52a62b1901f11aeaefa3480f88ee9770
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-04-11 22:49:04 +0200

    :art: Add editorconfig and format code

--- app/Http/Controllers/GifsController.php
+++ app/Http/Controllers/GifsController.php
@@ -12,14 +12,14 @@ class GifsController extends Controller
 	private $giphy;
 
 	public function __construct(GiphyRepository $giphy) {
-			$this->giphy = $giphy;
+		$this->giphy = $giphy;
 	}
 
 	public function trending(int $offset) {
 		return json_decode($this->giphy->trending(self::LIMIT, $offset))->data;
 	}
 
-  public function index() {
+	public function index() {
 		return view('index', [
 			'gifs' => json_decode($this->giphy->trending(self::LIMIT, 0))->data
 		]);
