9cb67154de5074695ad31efe621011e847feacff
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-05-17 22:54:16 +0200

    :hammer: Remove code to complete

--- resources/assets/js/components/SearchBar.vue
+++ resources/assets/js/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <form>
     <div class="form-group">
-      <input class="form-control" type="text" name="search" v-model="search" placeholder="Search" />
+      <input class="form-control" type="text" name="search" placeholder="Search" />
     </div>
   </form>
 </template>
@@ -9,7 +9,6 @@
 <script>
 import debounce from 'lodash/debounce'
 import pick from 'lodash/pick'
-import store from '../store.js'
 
 export default {
   props: {
@@ -17,17 +16,6 @@ export default {
       type: String,
       default: '',
     }
-  },
-  data() {
-    return {
-      sharedState: store.state,
-      search: this.initialSearch
-    }
-  },
-  watch: {
-    search: debounce(function startSearch(newValue) {
-      this.$emit('search', newValue)
-    }, 500),
   },
 }
 </script>
