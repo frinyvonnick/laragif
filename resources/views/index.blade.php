e8256df2706b40443114325a07835ac0de50fe46
FRIN Yvonnick <frin.yvonnick@gmail.com>
2017-05-17 23:34:58 +0200

    :hammer: Remove code to complete

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,4 @@
 @extends('layouts.app')
 
 @section('content')
-	@foreach ($gifs as $gif)
-		<img src="{{$gif->url}}" />
-	@endforeach
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,3 @@
 |
 */
 $gifs = json_decode(file_get_contents(base_path() . '/resources/mocks/search/cat/0.json'));
-
-Route::get('/', function() use($gifs) {
-  return view('index', [
-    'endpoint' => '/api/trending/',
-    'gifs' => $gifs
-  ]);
-});
-
-Route::get('/trending', function() use($gifs) {
-  $file = base_path() . '/resources/mocks/search/' . $term . '/' . $offset . '.json';
-
-  if (file_exists($file)) {
-      return json_decode(file_get_contents($file));
-  }
-
-  // Fallback on first json mock if we don't find a mock file
-  return $gifs;
-});
-Route::get('/search/{search}', 'SearchController@index');
-Route::middleware('auth')->get('/favorites', 'FavoritesController@index');
