815b8dd721f429575e41db4c18fe26e9c43634e6
benjamin <benjamin.cavy@gmail.com>
2017-05-19 10:42:51 +0200

    Add Application.vue file

---
+++ resources/assets/js/Application.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img v-for="gif in gifs" v-bind:src="gif.url" />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      gifs: {
+        type: Array,
+        default: []
+      }
+    },
+  }
+</script>
