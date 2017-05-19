725f16b1deff6bb479e3964e27b7f3b91fda76e5
benjamin <benjamin.cavy@gmail.com>
2017-05-19 17:39:54 +0200

    Add github authentication

--- config/services.php
+++ config/services.php
@@ -39,5 +39,11 @@ return [
         'client_id' => env('TWITTER_KEY'),
         'client_secret' => env('TWITTER_SECRET'),
         'redirect' => env('TWITTER_CALLBACK'),
+    ],
+
+    'github' => [
+        'client_id' => env('GITHUB_KEY'),
+        'client_secret' => env('GITHUB_SECRET'),
+        'redirect' => env('GITHUB_CALLBACK'),
     ]
 ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -71,6 +71,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <a href="{{ url('/auth/twitter') }}" class="btn btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
+                                <a href="{{ url('/auth/github') }}" class="btn btn-github"><i class="fa fa-github"></i> Github</a>
                             </div>
                         </div>
                     </form>
