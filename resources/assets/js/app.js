6e8acac3504045d1449ce7b5df73d912c1c657c3
benjamin <benjamin.cavy@gmail.com>
2017-05-19 10:40:57 +0200

    Completed code

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,9 +5,15 @@
  */
 
 import './bootstrap'
+import Application from './Application.vue'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+new Vue({
+  el: '#app',
+  components: { Application }
+})
