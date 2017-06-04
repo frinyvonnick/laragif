6b61322ffdfa313f68dee6726b0143b33409a251
benjamin <benjamin.cavy@gmail.com>
2017-06-04 17:21:48 +0200

    Make it work !

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,5 @@
 import Application from './components/Application.vue'
 import SearchBar from './components/SearchBar.vue'
-import Echo from 'laravel-echo'
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -15,11 +14,6 @@ import './bootstrap'
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
- window.Echo = new Echo({
-     broadcaster: 'socket.io',
-     host: window.echoServer
- });
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
--- resources/assets/js/components/SearchBar.vue
+++ resources/assets/js/components/SearchBar.vue
@@ -1,14 +1,13 @@
 <template>
   <form>
     <div class="form-group">
-      <input class="form-control" type="text" name="search" placeholder="Search"/>
+      <input class="form-control" type="text" name="search" placeholder="Search" />
     </div>
   </form>
 </template>
 
 <script>
 import debounce from 'lodash/debounce'
-import pick from 'lodash/pick'
 
 export default {
 }
